850d1a6c495182778800e06720bebaf95158bf73
Manuel Malvar Tombo <mmalvartombo@gmail.com>
2024-09-23 12:03:52 +0200

    Initial Proyect

---
+++ README.md
@@ -0,0 +1,3 @@
+# Despliegue de Aplicaciones Web
+## UD 1. Control de versiones (GIT)
+- Este es un arhcivo de ejemplo para el ejercicio 1 de Despliegue de Aplicaciones Web.
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio GIT</title>
+</head>
+<body>
+    <?php 
+        echo "<h1>Ejercicio GIT</h1>";
+    ?>
+</body>
+</html>
